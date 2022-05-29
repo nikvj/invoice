0e1b247409a01ba4d1a869665ae78e0b235dabfe
Nikhil <choudharyn693@gmail.com>
2022-05-29 17:45:43 +0530

    added quantity input and amount

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -14,6 +14,9 @@ export default function Dashboard() {
   let navigate = useNavigate();
   const [selectedProduct, setSelectedProduct] = useState([]);
   const { control, handleSubmit } = useForm(null);
+  const [code, setCode] = useState("");
+  const [name, setName] = useState("");
+  const [contact, setContact] = useState("");
 
   useEffect(() => {
     if (!localStorage.getItem("token")) {
@@ -21,16 +24,17 @@ export default function Dashboard() {
     }
   }, []);
 
-  const [code, setCode] = useState("");
   const handleClick = (e) => {
-    e.preventDefault();
-    axios.get(SEARCHBYCODE + code).then((response) => {
-      if (response) {
-        const newItem = [...selectedProduct];
-        newItem.push(response.data.product);
-        setSelectedProduct(newItem);
-      }
-    });
+    if (e.key === "Enter") {
+      e.preventDefault();
+      axios.get(SEARCHBYCODE + code).then((response) => {
+        if (response) {
+          const newItem = [...selectedProduct];
+          newItem.push(response.data.product);
+          setSelectedProduct(newItem);
+        }
+      });
+    }
   };
 
   const formatCurrency = (value) => {
@@ -44,6 +48,55 @@ export default function Dashboard() {
     return formatCurrency(rowData.price);
   };
 
+  const [quantity, setQuantity] = useState(1);
+  const [amount, setAmount] = useState(0);
+  const handleQuantity = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      const price = selectedProduct.map((item) => {
+        return item.price;
+      });
+      const amount = calculateAmount(quantity, price);
+      setAmount(amount);
+    }
+  };
+
+  const calculateAmount = (quantity, price) => {
+    return quantity * price;
+  };
+
+  const quantityBodyTemplate = () => {
+    return (
+      <React.Fragment>
+        <span className="p-float-label">
+          <Controller
+            name="quantity"
+            control={control}
+            render={({ field, fieldState }) => (
+              <InputText
+                id={field.quantity}
+                {...field}
+                autoFocus
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+                onKeyPress={handleQuantity}
+                style={{ width: "50px" }}
+              />
+            )}
+          />
+        </span>
+      </React.Fragment>
+    );
+  };
+
+  const amountBodyTemplate = () => {
+    return (
+      <React.Fragment>
+        <h4 style={{ position: "center" }}>{formatCurrency(amount)}</h4>
+      </React.Fragment>
+    );
+  };
+
   return (
     <div>
       <Appbar />
@@ -53,10 +106,10 @@ export default function Dashboard() {
             classNamoe="flex align-items-center justify-content-center"
             size={300}
           >
-            <div style={{ padding: "3%", height: "15%" }}>
-              <div className="card">
+            <div style={{ padding: "5%", height: "15%" }}>
+              <div className="card grid p-fluid">
                 <form className="p-fluid">
-                  <div className="field">
+                  <div className="grid p-fluid">
                     <span className="p-float-label">
                       <Controller
                         name="code"
@@ -69,24 +122,66 @@ export default function Dashboard() {
                             autoFocus
                             value={code}
                             onChange={(e) => setCode(e.target.value)}
-                            style={{ width: "30%" }}
+                            onKeyPress={handleClick}
+                            style={{ width: "70%" }}
                           />
                         )}
                       />
                       <label htmlFor="code">
                         Product Code<span style={{ color: "red" }}>*</span>
                       </label>
-
-                      <Button
-                        type="submit"
-                        label="Submit"
-                        className="mt-2"
-                        onClick={handleClick}
-                        style={{ width: "20%", marginLeft: "20px" }}
+                    </span>
+                    <span
+                      className="p-float-label"
+                      style={{ marginLeft: "-30px" }}
+                    >
+                      <Controller
+                        name="name"
+                        control={control}
+                        rules={{ required: "Code is required." }}
+                        render={({ field, fieldState }) => (
+                          <InputText
+                            id={field.name}
+                            {...field}
+                            autoFocus
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            onKeyPress={handleClick}
+                            style={{ width: "70%" }}
+                          />
+                        )}
                       />
+                      <label htmlFor="name">Customer Name</label>
+                    </span>
+                    <span
+                      className="p-float-label"
+                      style={{ marginLeft: "-30px" }}
+                    >
+                      <Controller
+                        name="name"
+                        control={control}
+                        rules={{ required: "Code is required." }}
+                        render={({ field, fieldState }) => (
+                          <InputText
+                            id={field.contact}
+                            {...field}
+                            autoFocus
+                            value={contact}
+                            onChange={(e) => setContact(e.target.value)}
+                            onKeyPress={handleClick}
+                            style={{ width: "80%" }}
+                          />
+                        )}
+                      />
+                      <label htmlFor="name">
+                        Customer Contact<span style={{ color: "red" }}>*</span>
+                      </label>
                     </span>
                   </div>
                 </form>
+                <h3 style={{ marginTop: "5px" }}>
+                  Total Amount: {formatCurrency(amount)}
+                </h3>
               </div>
             </div>
 
@@ -118,11 +213,13 @@ export default function Dashboard() {
                   <Column
                     field="buying_quantity"
                     header="Quantity"
+                    body={quantityBodyTemplate}
                     style={{ minWidth: "8rem" }}
                   ></Column>
                   <Column
-                    field="buying_price"
-                    header="Total"
+                    field="buying_amount"
+                    header="Amount"
+                    body={amountBodyTemplate}
                     style={{ minWidth: "8rem" }}
                   ></Column>
                 </DataTable>
